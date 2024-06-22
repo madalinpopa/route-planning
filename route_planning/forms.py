c0a8785b18b0ed38005bca1a318e1318fb601859
Madalin Popa <contact@madalinpopa.com>
2024-06-23 00:45:18 +0300

    feat: Add LoginForm to handle user authentication

--- route_planning/forms.py
+++ route_planning/forms.py
@@ -3,6 +3,11 @@ from wtforms import StringField
 from wtforms.validators import DataRequired
 
 
+class LoginForm(FlaskForm):
+    username = StringField("Username", validators=[DataRequired()])
+    password = StringField("Password", validators=[DataRequired()])
+
+
 class UserRegistrationForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     password = StringField("Password", validators=[DataRequired()])
