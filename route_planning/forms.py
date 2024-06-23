8d877e84eeb84706148cf3ca78f070b12139a003
Madalin Popa <contact@madalinpopa.com>
2024-06-23 17:43:57 +0300

    feat: Added forms for Company, Driver, Vehicle, and Route in forms.py.

--- route_planning/forms.py
+++ route_planning/forms.py
@@ -12,3 +12,39 @@ class UserRegistrationForm(FlaskForm):
     username = StringField("Username", validators=[DataRequired()])
     password = StringField("Password", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
+
+
+class CompanyForm(FlaskForm):
+    vat = StringField("VAT", validators=[DataRequired()])
+    name = StringField("Name", validators=[DataRequired()])
+    address = StringField("Address", validators=[DataRequired()])
+
+
+class DriverForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    surname = StringField("Surname", validators=[DataRequired()])
+    email = StringField("Email", validators=[DataRequired()])
+    phone = StringField("Phone", validators=[DataRequired()])
+    company = StringField("Company", validators=[DataRequired()])
+
+
+class VehicleForm(FlaskForm):
+    registration = StringField("Registration", validators=[DataRequired()])
+    make = StringField("Make", validators=[DataRequired()])
+    model = StringField("Model", validators=[DataRequired()])
+    year = StringField("Year", validators=[DataRequired()])
+    company = StringField("Company", validators=[DataRequired()])
+
+
+class RouteForm(FlaskForm):
+    start = StringField("Start", validators=[DataRequired()])
+    end = StringField("End", validators=[DataRequired()])
+    distance = StringField("Distance", validators=[DataRequired()])
+    company = StringField("Company", validators=[DataRequired()])
+    price = StringField("Price", validators=[DataRequired()])
+    date = StringField("Date", validators=[DataRequired()])
+    time = StringField("Time", validators=[DataRequired()])
+    status = StringField("Status", validators=[DataRequired()])
+    driver = StringField("Driver", validators=[DataRequired()])
+    vehicle = StringField("Vehicle", validators=[DataRequired()])
+    notes = StringField("Notes", validators=[DataRequired()])
