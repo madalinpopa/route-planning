700a3be3d90be500f4dd572f7f5771550d525252
Madalin Popa <contact@madalinpopa.com>
2024-06-23 20:12:32 +0300

    feat: Add Company model to details route in company view

--- route_planning/views/company.py
+++ route_planning/views/company.py
@@ -1,8 +1,11 @@
 from flask import Blueprint, render_template
+from ..models import Company
+from .. import db
 
 company = Blueprint("company", __name__, url_prefix="/company")
 
 
-@company.route("/details")
+@company.route("/details", methods=["GET"])
 def details():
-    return render_template("company/details.html")
+    default_company = Company.query.first()
+    return render_template("company/details.html", company=default_company)
