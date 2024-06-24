43b3874e1a92c9c62c815fb81c0de32f0dd768a8
Madalin Popa <contact@madalinpopa.com>
2024-06-24 14:56:08 +0300

    feat: Add route for adding a vehicle with form validation.

--- route_planning/views/vehicle.py
+++ route_planning/views/vehicle.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template
+from ..forms import VehicleForm
 
 vehicle = Blueprint("vehicle", __name__, url_prefix="/vehicle")
 
@@ -6,3 +7,11 @@ vehicle = Blueprint("vehicle", __name__, url_prefix="/vehicle")
 @vehicle.route("/list")
 def vehicle_list():
     return render_template("vehicle/list.html")
+
+
+@vehicle.route("/add", methods=["GET", "POST"])
+def vehicle_add():
+    form = VehicleForm()
+    if form.validate_on_submit():
+        return "Vehicle added"
+    return render_template("vehicle/add.html", form=form)
