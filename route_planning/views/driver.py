d46c2aeaa3b8c804af6bce21c9b7261bdfbd253d
Madalin Popa <contact@madalinpopa.com>
2024-06-24 18:30:55 +0300

    feat: Add pagination to driver list page

--- route_planning/views/driver.py
+++ route_planning/views/driver.py
@@ -1,8 +1,14 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, current_app, render_template, request
+
+from ..models import Driver
 
 driver = Blueprint("driver", __name__, url_prefix="/driver")
 
 
 @driver.route("/list")
 def driver_list():
+    page = request.args.get("page", 1, type=int)
+    pagination = Driver.query.paginate(
+        page=page, per_page=current_app.config["ITEMS_PER_PAGE"], error_out=False
+    )
     return render_template("driver/list.html")
