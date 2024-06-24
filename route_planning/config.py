f478814c60b13b9290556484b27e0874e5c85e44
Madalin Popa <contact@madalinpopa.com>
2024-06-24 17:55:41 +0300

    feat: Add items per page setting to config

--- route_planning/config.py
+++ route_planning/config.py
@@ -4,6 +4,7 @@ import os
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY") or "dev"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    ITEMS_PER_PAGE = 5
 
 
 class DevelopmentConfig(Config):
