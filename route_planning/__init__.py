b2ac256d1f55e615d22d76fcdc0666f290a9363c
Madalin Popa <contact@madalinpopa.com>
2024-06-25 09:15:09 +0300

    🌐 Add flask-babel for localization and timezone support.

--- route_planning/__init__.py
+++ route_planning/__init__.py
@@ -1,7 +1,6 @@
 import os
 
-
-from flask import Flask
+from flask import Flask, g, request
 from flask.cli import AppGroup
 
 from flask_sqlalchemy import SQLAlchemy
@@ -11,17 +10,35 @@ from .config import config
 from flask_migrate import Migrate
 from flask_wtf.csrf import CSRFProtect
 from flask_caching import Cache
+from flask_babel import Babel
 
 
 class Base(DeclarativeBase):
     pass
 
 
+def get_locale():
+    # if a user is logged in, use the locale from the user settings
+    user = getattr(g, "user", None)
+    if user is not None:
+        return user.locale
+    # otherwise try to guess the language from the user accept
+    # header the browser transmits.  We support de/fr/en in this
+    # example.  The best match wins.
+    return request.accept_languages.best_match(["de", "fr", "en"])
+
+
+def get_timezone():
+    user = getattr(g, "user", None)
+    if user is not None:
+        return user.timezone
+
+
 db = SQLAlchemy(model_class=Base)
 migrate = Migrate()
 csrf = CSRFProtect()
 cache = Cache()
-
+babel = Babel()
 user_cli = AppGroup("user")
 
 
@@ -40,6 +57,7 @@ def create_app(test_config=None):
 
     db.init_app(app)
     csrf.init_app(app)
+    babel.init_app(app)
     cache.init_app(
         app,
         config={
