0ce7c790d4ec575e26c67ac0ac9a65bbbe539389
Madalin Popa <contact@madalinpopa.com>
2024-06-29 11:19:55 +0300

    🔧 Update route form to include QuerySelectField for vehicle and driver.

--- route_planning/forms.py
+++ route_planning/forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SelectField
+from wtforms import StringField, TextAreaField, SelectField, DateField, IntegerField
 from wtforms.validators import DataRequired, Email, ValidationError
+from wtforms_sqlalchemy.fields import QuerySelectField
 
-from .models import Vehicle
+
+from .models import Vehicle, Driver
 
 
 class LoginForm(FlaskForm):
@@ -51,10 +53,30 @@ class VehicleForm(FlaskForm):
 
 
 class RouteForm(FlaskForm):
-    date = StringField("Date", validators=[DataRequired()])
+    date = DateField("Date", validators=[DataRequired()])
     start_address = StringField("Start address", validators=[DataRequired()])
-    start_km = StringField("Start km", validators=[DataRequired()])
+    start_km = IntegerField("Start km", validators=[DataRequired()])
     end_address = StringField("End address", validators=[DataRequired()])
-    end_km = StringField("End km", validators=[DataRequired()])
-    vehicle = SelectField("Vehicle", validators=[DataRequired()])
-    driver = SelectField("Driver", validators=[DataRequired()])
+    end_km = IntegerField("End km", validators=[DataRequired()])
+    driver = QuerySelectField(
+        "Driver",
+        query_factory=lambda: Driver.query.all(),
+        allow_blank=True,
+        get_label="name",
+    )
+    vehicle = QuerySelectField(
+        "Vehicle",
+        query_factory=lambda: Vehicle.query.all(),
+        allow_blank=True,
+        get_label="plate",
+    )
+
+    # def __init__(self, *args, **kwargs):
+    #     super(RouteForm, self).__init__(*args, **kwargs)
+    #     self.vehicle.choices = [
+    #         (vehicle.id, vehicle.plate) for vehicle in Vehicle.query.order_by("plate")
+    #     ]
+    #     self.driver.choices = [
+    #         (driver.id, f"{driver.name} {driver.surname}")
+    #         for driver in Driver.query.order_by("name")
+    #     ]
