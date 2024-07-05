7e5e2c6b14c0e60f4fc6ab31936b940d6e4dcb37
Madalin Popa <contact@madalinpopa.com>
2024-07-05 09:19:46 +0300

    ✨ feat: Add default company creation for user on first login.

--- route_planning/views/main.py
+++ route_planning/views/main.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, g
 from .auth import login_required
 
 from .. import db
@@ -10,11 +10,13 @@ main = Blueprint("main", __name__)
 @main.route("/")
 @login_required
 def index():
-    default_company = Company.query.first()
-    if default_company is None:
-        new_company = Company(vat="123456789", name="Company Name")
+    user_companies = Company.query.filter_by(user_id=g.user.id).all()
+    if not user_companies:
+        new_company = Company(
+            vat="123456789", name="Default Company", user_id=g.user.id
+        )
         db.session.add(new_company)
         db.session.commit()
         return render_template("index.html", company=new_company)
     else:
-        return render_template("index.html", company=default_company)
+        return render_template("index.html", company=user_companies[0])
